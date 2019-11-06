2964503dfd696d4bdbbc05f792ba1a831c8d119f
cool_tool@yahoo.com <cool_tool@yahoo.com>
2019-11-06 18:37:26 +0200

    Quick search dynamic button added, some merge conflicts resolved

--- CocaColaFinalProject/Areas/Cocktails/Models/CocktailViewModel.cs
+++ CocaColaFinalProject/Areas/Cocktails/Models/CocktailViewModel.cs
@@ -29,6 +29,7 @@ namespace CM.Web.Areas.Cocktails.Models
         public decimal Rating { get; set; }
 
         public IFormFile CocktailImage { set; get; }
+        public string headLetter { set; get; }
 
         public List<SelectListItem> Ingredients { get; set; } = new List<SelectListItem>();
         public List<string> IngredientsIDs { get; set; } = new List<string>();
